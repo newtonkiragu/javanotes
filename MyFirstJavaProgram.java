648bc51d57486817c7cb58a267dd3bec389a4e9c
Newton Karanu <Retina15@karanu.local>
2019-03-18 16:40:06 +0300

    created my first java program

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,6 @@
+public class MyFirstJavaProgram {
+//    This is my first java program
+    public static void main(String []args) {
+        System.out.println("hello world"); // prints hello world
+    }
+}
